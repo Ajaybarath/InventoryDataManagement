165992319d00974b60b6b3ca0aaf26da02536082
ajaybarath <ajaybarath3@gmail.com>
2021-09-05 16:51:03 +0530

    ADDED: getter and setter function for inventory

---
+++ src/com/bridgeLabz/dataManagement/Inventory.java
@@ -0,0 +1,40 @@
+package com.bridgeLabz.dataManagement;
+
+public class Inventory {
+
+	String name;
+	String weight;
+	int pricePerKg;
+
+	public Inventory(String name, String weight, int pricePerKg) {
+		super();
+		this.name = name;
+		this.weight = weight;
+		this.pricePerKg = pricePerKg;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getWeight() {
+		return weight;
+	}
+
+	public int getPricePerKg() {
+		return pricePerKg;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+	public void setPricePerKg(int pricePerKg) {
+		this.pricePerKg = pricePerKg;
+	}
+
+}
